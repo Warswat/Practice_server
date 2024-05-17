3eeebef63fa60a0f92e6de5709bd5b5f62dc216c
Warswat <warswat228@gmail.com>
2024-05-17 21:53:40 +0300

    Initial commit

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для продукта
+    class Meta:
+        model = Product
+        fields = ['title','description']
+
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+    pass
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+        for position in positions:
+            position['stock']=stock
+            StockProduct(**position).save()
+
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=position['product'], defaults={'price': position['price'], 'quantity': position['quantity']})
+
+        return stock
+    class Meta:
+        model = Stock
+        fields = '__all__'
---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,24 @@
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title','description']
+    pagination_class = LimitOffsetPagination
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['=positions__product__id']
+    pagination_class = LimitOffsetPagination
